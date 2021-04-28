78bef9251ee7383b506b44289998c1e51dcf6704
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-27 21:31:16 -0700

    add the rest of the tests and functions ahead of time

--- src/array-filter.js
+++ src/array-filter.js
@@ -5,3 +5,15 @@ export function fiveAndGreaterOnly(arr) {
 export function evensOnly(arr) {
   return arr.filter(n => n % 2 === 0);
 }
+
+export function fiveCharactersOrFewerOnly(arr){
+  
+}
+
+export function peopleWhoBelongToTheIlluminati(arr){
+  
+}
+
+export function ofAge(arr){
+  
+}
--- src/array-filter.test.js
+++ src/array-filter.test.js
@@ -1,4 +1,4 @@
-import { fiveAndGreaterOnly, evensOnly } from './array-filter';
+import { fiveAndGreaterOnly, evensOnly, fiveCharactersOrFewerOnly, peopleWhoBelongToTheIlluminati, ofAge } from './array-filter';
 
 test('Five and greater only', () => {
   const input = [3, 6, 8, 2]; // arrange
@@ -10,4 +10,41 @@ test('evens only', () => {
   const input = [3, 6, 8, 2]; // arrange
   const output = evensOnly(input); // act
   expect(output).toEqual([6, 8, 2]); // assert
+});
+
+test('five characters or fewer only', () => {
+  const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping']; // arrange
+  const output = fiveCharactersOrFewerOnly(input); // act
+  expect(output).toEqual(['by', 'dog', 'wolf', 'eaten']); // assert
+});
+
+test('people who belong to the illuminati', () => {
+  const input = [
+    { name: 'Angelina Jolie', member: true },
+    { name: 'Eric Jones', member: false },
+    { name: 'Paris Hilton', member: true },
+    { name: 'Kayne West', member: false },
+    { name: 'Bob Ziroll', member: true }
+  ]; // arrange
+  const output = peopleWhoBelongToTheIlluminati(input); // act
+  expect(output).toEqual([ 
+    { name: 'Angelina Jolie', member: true }, 
+    { name: 'Paris Hilton', member: true }, 
+    { name: 'Bob Ziroll', member: true } 
+  ]); // assert
+});
+
+test('of age', () => {
+  const input = [
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Eric Jones', age: 2 },
+    { name: 'Paris Hilton', age: 5 },
+    { name: 'Kayne West', age: 16 },
+    { name: 'Bob Ziroll', age: 100 }
+  ]; // arrange
+  const output = ofAge(input); // act
+  expect(output).toEqual([ 
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Bob Ziroll', age: 100 } 
+  ]); // assert
 });
