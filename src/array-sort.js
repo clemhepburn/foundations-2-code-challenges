55ee3a3992b0ca6fa3424353fa74c9d7e5799c30
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-27 21:41:34 -0700

    set up sort function templates

---
+++ src/array-sort.js
@@ -0,0 +1,19 @@
+export function leastToGreatest(arr){
+  return arr.sort();
+}
+
+export function greatestToLeast(arr){
+  return arr.sort();
+}
+
+export function lengthSort(arr){
+  return arr.sort();
+}
+
+export function alphabetical(arr){
+  return arr.sort();
+}
+
+export function byAge(arr){
+  return arr.sort();
+}
