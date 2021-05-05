633a8a6c04e00cb14216e071177a19e8f0e3cb35
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-05 07:55:23 -0700

    test houseSize function. oof

--- src/object-keys-entries.js
+++ src/object-keys-entries.js
@@ -5,3 +5,18 @@ export function getHouses(arr) {
 export function updateNumbers(obj) {
   return Object.keys(obj).map((key) => (key + ': ' + obj[key]));
 }
+
+export function houseSize(arr) {
+  let newArr = [];
+  for (let item of arr) {
+    newArr.push(item.name);
+    if (item.spouse !== null) {
+      newArr.push(item.spouse);
+    } if (item.children != null) {
+      for (let child of item.children) {
+        newArr.push(child);
+      }
+    }
+  }
+  return newArr.length;
+}
